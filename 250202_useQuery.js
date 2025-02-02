0b7abb10e2620e954eca337edfda834057ec1dac
jodasom <jodasom0216@gmail.com>
2025-02-02 17:39:47 +0900

    useQuery

--- 250202_useQuery.js
+++ 250202_useQuery.js
@@ -10,6 +10,8 @@ const MyComponent = () => {
     const { data, isLoading, error } = useQuery({
         queryKey: ["posts"],
         queryFn: fetchData,
+        staleTime : 1000 * 60 * 5, //5분 동안 fresh 상태 유지
+        cacheTime : 1000 * 60 *10 // 10분 후 캐시 삭제
     })
 
     if (isLoading) return <p>lading.....</p>;
@@ -22,4 +24,21 @@ const MyComponent = () => {
         ))}
         </div>
     )
+}
+
+//쿼리변수 활용
+const fetchUser = async (userId) => {
+    const {data} = await axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`);
+    return data;
+};
+
+const UserProfile = ({userId}) => {
+    const {data, isLoading} = useQuery({
+        queryKey : ["user", userId], //userId마다 다른 데이터를 캐싱
+        queryFn: () => fetchUser(userId)
+    });
+
+    if(isLoading) return <p>loading....</p>
+
+    return <p>{data.name}</p>
 }
